3807f5792096ee3eead80458913bea10df12c17c
Andrew Briscoe <admin@pandasportal.net>
2023-11-27 23:26:54 +0800

    Added requirements.txt

---
+++ demo.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+client = OpenAI()
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo", # gpt-4-1106-preview
+  messages=[
+    {"role": "system", "content": "You provide answer, solutions, documentation, insights, code review and engage in pair programming."},
+    {"role": "user", "content": "List and describe the solid principles. You will demonstrate how the solid principles can be represented in category theory. You will then demonstrate provide examples for each of the SOLID principles by providing code examples in python, and in another way that achieves the same result but using a functional style to demonstrate how category theory is able to achieve the same result using functions as first class objects."}
+  ]
+)
+
+print(completion.choices[0].message.content)
